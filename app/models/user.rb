de5b68d791b2248423f875a52246c27c880ebcd8
Oliver Shaw <oliver.shaw@sage.com>
2013-10-28 15:12:42 +0000

    finished chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,12 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  has_secure_password
+  validates :password_confirmation, presence: true
+  attr_accessible :email, :name, :password, :password_confirmation
   validates :name, presence: true, length: {maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates_confirmation_of :password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
     uniqueness: {case_sensitive: false}
