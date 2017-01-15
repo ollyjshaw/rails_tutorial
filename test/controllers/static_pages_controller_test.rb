86ef0f5ac7ab263217a550f9e916091dba3b734f
Olly Shaw <ollyjshaw@gmail.com>
2017-01-15 20:38:54 +0000

    up to 5.3.2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   def setup
     @base_title = "Ruby on Rails Tutorial"
-
   end
+
   test "should get root" do
     get static_pages_home_url
     assert_response :success
@@ -26,5 +27,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
