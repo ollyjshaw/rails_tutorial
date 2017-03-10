dfaf15a9a005f503166eecffcc67bbaf66f3b2fb
Olly Shaw <ollyjshaw@gmail.com>
2017-03-10 21:36:05 +0000

    ready for 12.3

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -11,6 +11,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
     UserMailer.password_reset
+    user.account_activation = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
