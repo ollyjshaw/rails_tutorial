42b56806cdf6a86f94c4353051a9fbeca90e0790
Olly Shaw <ollyjshaw@gmail.com>
2017-03-11 20:05:20 +0000

    ready for 13.2.1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,6 +72,7 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return falsse for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
   test "email should reject an invalid email" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                                foo@bar_baz.com foo@bar+baz.com foo@example..com]
@@ -80,4 +81,13 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_email.inspect} should be invalid"
     end
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
