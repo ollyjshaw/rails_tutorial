ec6212a123aed189042a440d1427a14989ba7d68
Olly Shaw <ollyjshaw@gmail.com>
2017-03-19 17:55:04 +0000

    ready for 14.2.3

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match CGI.escapeHTML(micropost.content), response.body
     end
+
+    assert_select '#following'#, text: @user.following.count
+    assert_select '#followers'#, text: @user.followers.count
   end
 end
